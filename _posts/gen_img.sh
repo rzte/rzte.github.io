4ddd2a3b22966c9e33931b2ea6eb8ab44f57bcb3
r6 <1574805581@qq.com>
2018-12-16 15:38:23 +0800

    bak

---
+++ _posts/gen_img.sh
@@ -0,0 +1,34 @@
+#! /bin/bash
+
+
+if [ $# != 1 ];then
+	echo "$0 xxx.img"
+	exit
+fi
+
+ls $1 &>> /dev/null
+
+if [ $? != 0 ];then
+	echo "$1 not exists!"
+	exit
+fi
+
+filepath="../assets/img/md/"
+
+filename="`date +%F`-$RANDOM$RANDOM.img"
+
+ls $filepath/$filename &>> /dev/null
+
+while [ $? -eq 0 ];do
+	filename="`date +%F`-$RANDOM$RANDOM.img"
+done
+
+file="${filepath}${filename}"
+
+echo "filepath: $file"
+
+mv $1 $file
+
+if [ $? -eq 0 ];then
+	echo "ok"
+fi
